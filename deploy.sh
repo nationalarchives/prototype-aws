9016880ebaf9e90e32cecc447b63c7acfb3db2ba
Sam Palmer <github@sampalmer.dev>
2019-05-21 14:57:24 +0100

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+zip tdr-run-tasks.zip tdr-run-tasks.py
+aws s3 cp tdr-run-tasks.zip s3://tdr-run-tasks
+aws lambda update-function-code --function-name tdr-run-tasks --s3-bucket tdr-run-tasks --s3-key tdr-run-tasks.zip
---
+++ tdr-run-tasks.py
@@ -0,0 +1,21 @@
+import json
+import boto3
+import os
+
+def lambda_handler(event, context):
+    sqs_client = boto3.client('sqs')
+    sqs_client.send_message(QueueUrl=os.environ['QUEUE_URL'] ,MessageBody=json.dumps(event))
+    ecs_client = boto3.client('ecs')
+    ecs_client.run_task(taskDefinition=os.environ['TASK_ARN'], launchType="FARGATE", networkConfiguration={
+        'awsvpcConfiguration': {
+            'subnets': [
+                os.environ['SUBNET_ID'],
+            ],
+            'securityGroups': [
+                os.environ['SECURITY_GROUP_ID'],
+            ],
+            'assignPublicIp': 'ENABLED'
+        }
+    })
+
+
