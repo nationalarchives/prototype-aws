bc3247dfba4cdcdb4e7cb037765e3a20793ce256
Sam Palmer <github@sampalmer.dev>
2019-06-05 15:42:07 +0100

    add region to the prod clients

--- tdr/runtasks.py
+++ tdr/runtasks.py
@@ -4,11 +4,11 @@ import os
 
 
 def lambda_handler(event, context):
-    sqs_client = boto3.client('sqs')
+    sqs_client = boto3.client('sqs', region_name='eu-west-2')
     message = sqs_client.send_message(
         QueueUrl=os.environ['QUEUE_URL'], MessageBody=json.dumps(event))
 
-    ecs_client = boto3.client('ecs')
+    ecs_client = boto3.client('ecs', region_name='eu-west-2')
 
     ecs_client.run_task(cluster='default', taskDefinition=os.environ['TASK_ARN'], launchType="FARGATE", networkConfiguration={
         'awsvpcConfiguration': {
